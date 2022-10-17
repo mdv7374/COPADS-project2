24dc429e0395e0ac4a09bcc7dce20cc2dc14e145
Michael Vogt <mdv7374@rit.edu>
2022-10-17 15:23:29 -0400

    added in parallel code and additional checks for isnotprime, still need to add comments and double check isprobablyprime

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,16 +8,35 @@ namespace Project2
 {
     public static class ExtensionMethods
     {
-        public static bool isNotPrime(this BigInteger i)
+        public static bool IsNotPrime(this BigInteger i)
         {
+            //list of all primes from 2 - 1000
+            int[] primes = new int[]{3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83,
+                89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
+                197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313,
+                317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443,
+                449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587,
+                593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719,
+                727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859,
+                863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997};
+            //if it is negative, it is not a prime
             if (i < 0)
             {
                 return true;
             }
-
-            if (i % 2 == 0)
+            //if it is even, it is not a prime number
+            if (i.IsEven)
             {
                 return true;
+            }
+
+            //divide the value by all primes less than 1000, if mod equals 0, it is a prime number
+            foreach (var prime in primes)
+            {
+                if (i % prime == 0)
+                {
+                    return true;
+                }
             }
             
             return false;
@@ -88,21 +107,22 @@ namespace Project2
     {
         public BigInteger GenerateBigInt(int bytecount)
         {
-            BigInteger newBigInt;
+            BigInteger newBigInt = new BigInteger(0);
             RandomNumberGenerator rng = RandomNumberGenerator.Create();
-            while (true)
+            Parallel.For(0, Int32.MaxValue, (i,state) =>
             {
                 byte[] bytearr = new byte[bytecount];
                 rng.GetBytes(bytearr);
-                newBigInt = new BigInteger(bytearr);
-                if (!newBigInt.isNotPrime())
+                BigInteger temp = new BigInteger(bytearr);
+                if (!temp.IsNotPrime())
                 {
-                    if (newBigInt.IsProbablyPrime())
+                    if (temp.IsProbablyPrime())
                     {
-                        break;
+                        newBigInt = temp;
+                        state.Stop();
                     }
                 }
-            }
+            });
             return newBigInt;
         }
 
